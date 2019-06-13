c5af66cf214240a30c532e62dc437664393edd5c
dwedzikowski <44393596+dwedzikowski@users.noreply.github.com>
2019-06-14 00:06:34 +0200

    Add files via upload
    Updated the model loader so that it actually works

--- src/main/java/Entities/Model.java
+++ src/main/java/Entities/Model.java
@@ -13,14 +13,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Model implements FileLoader {
-    private List<Vector3f> temp_vertices = new ArrayList<Vector3f>();
-    public List<Vector3f> temp_normals = new ArrayList<Vector3f>();
-    private List<Vector2f> temp_texcoords = new ArrayList<Vector2f>();
-    private List<Integer> temp_indices = new ArrayList<Integer>();
-    public float[] vertices = {};
-    public float[] normals = {};
-    public float[] texcoords = {};
-    public int[] indices = {};
+    private List<Vector3f> vertices = new ArrayList<Vector3f>();
+    private List<Vector3f> normals = new ArrayList<Vector3f>();
+    private List<Vector2f> texcoords = new ArrayList<Vector2f>();
+
+    private List<Integer> face_vertices = new ArrayList<Integer>();
+    private List<Integer> face_texcoords = new ArrayList<Integer>();
+    private List<Integer> face_normals = new ArrayList<Integer>();
+
+    public float[] ready_vertices;
+    public float[] ready_texcoords;
+    public float[] ready_normals;
+
     public FloatBuffer fbVertex;
     public FloatBuffer fbVertexNormals;
     public FloatBuffer fbTexCoords;
@@ -36,9 +40,13 @@ public class Model implements FileLoader {
     public void load(String name){
 
         try{
-            temp_vertices.clear();
-            temp_normals.clear();
-            temp_texcoords.clear();
+            vertices.clear();
+            normals.clear();
+            texcoords.clear();
+
+            face_vertices.clear();
+            face_texcoords.clear();
+            face_normals.clear();
 
             BufferedReader r = new BufferedReader(
                     new FileReader(new File(name)));
@@ -46,67 +54,80 @@ public class Model implements FileLoader {
             while(true) {
                 cur = r.readLine().split(" ");
                 if (cur[0].equals("v")) {
-                    temp_vertices.add(new Vector3f(
+                    vertices.add(new Vector3f(
                             Float.parseFloat(cur[1]),
                             Float.parseFloat(cur[2]),
                             Float.parseFloat(cur[3])));
                 }
                 if (cur[0].equals("vn")) {
-                    temp_normals.add(new Vector3f(
+                    normals.add(new Vector3f(
                             Float.parseFloat(cur[1]),
                             Float.parseFloat(cur[2]),
                             Float.parseFloat(cur[3])));
                 }
                 if (cur[0].equals("vt")) {
-                    temp_texcoords.add(new Vector2f(
+                    texcoords.add(new Vector2f(
                             Float.parseFloat(cur[1]),
                             Float.parseFloat(cur[2])));
                 }
-                if (cur[0].equals("s")) {
-                    vertices = new float[temp_vertices.size() * 4];
-                    normals = new float[temp_vertices.size() * 4];
-                    texcoords = new float[temp_vertices.size() * 2];
-                    VertexCount = temp_vertices.size();
+                if (cur[0].equals("s"))
                     break;
-                }
             }
                 String line;
                 int m = 0;
                 int n = 0;
                 while((line = r.readLine()) != null){
-                    for(int i = 3; i > 0; i--) {        //change to for(int i=0;i<4;i++) if the face culling works in the wrong way
-                        cur = line.split(" ")[i].split("/");
-                        help(cur,m,n);
-                        m+=4;
-                        n+=2;
-                    }
+                    if(line.startsWith("f"))
+                        for(int i = 1; i < 4; i++) {
+                            cur = line.split(" ")[i].split("/");
+                            help(cur);
+                        }
                 }
+                VertexCount = face_vertices.size();
+                finish();
             r.close();
 
         }catch(Exception e){
             e.printStackTrace();
         }
-        fbVertex = makeFloatBuffer(vertices);
-        fbVertexNormals = makeFloatBuffer(normals);
-        fbTexCoords = makeFloatBuffer(texcoords);
+        //fbVertex = makeFloatBuffer(ready_vertices);
+        //fbVertexNormals = makeFloatBuffer(ready_normals);
+        //fbTexCoords = makeFloatBuffer(ready_texcoords);
     }
-    private void help(String [] cur, int pos, int texpos){
-        Vector3f curVertex = temp_vertices.get(Integer.parseInt(cur[0])-1);
-        vertices[pos]=curVertex.x;
-        vertices[pos+1]=curVertex.y;
-        vertices[pos+2]=curVertex.z;
-        vertices[pos+3]=1.0f;
-        Vector3f curNormal = temp_normals.get(Integer.parseInt(cur[2])-1);
-        normals[pos]=curNormal.x;
-        normals[pos+1]=curNormal.y;
-        normals[pos+2]=curNormal.z;
-        normals[pos+3]=0.0f;
-        if (!cur[1].equals("")) {
-            Vector2f curTexcoord = temp_texcoords.get(Integer.parseInt(cur[1]) - 1);
-            texcoords[texpos] = curTexcoord.x;
-            texcoords[texpos + 1] = curTexcoord.y;
+    private void help(String [] cur){
+        face_vertices.add(Integer.parseInt(cur[0]));
+        face_normals.add(Integer.parseInt(cur[2]));
+        if (!cur[1].equals(""))
+            face_texcoords.add(Integer.parseInt(cur[1]));
+    }
+    private void finish(){
+        ready_vertices = new float[4*VertexCount];
+        ready_texcoords = new float[2*VertexCount];
+        ready_normals = new float[4*VertexCount];
+
+        for (int i = 0; i < VertexCount; i++){
+            Vector3f cur_vert = vertices.get(face_vertices.get(i) - 1);
+            ready_vertices[(4*i)] = cur_vert.x;
+            ready_vertices[(4*i)+1] = cur_vert.y;
+            ready_vertices[(4*i)+2] = cur_vert.z;
+            ready_vertices[(4*i)+3] = 1.0f;
+
+            Vector2f cur_tex = texcoords.get(face_texcoords.get(i) - 1);
+            ready_texcoords[(2*i)] = cur_tex.x;
+            ready_texcoords[(2*i)+1] = 1 - cur_tex.y;
+
+            Vector3f cur_norm = normals.get(face_normals.get(i) - 1);
+            ready_normals[(4*i)] = cur_norm.x;
+            ready_normals[(4*i)+1] = cur_norm.y;
+            ready_normals[(4*i)+2] = cur_norm.z;
+            ready_normals[(4*i)+3] = 0.0f;
         }
+        fbVertex = makeFloatBuffer(ready_vertices);
+        fbTexCoords = makeFloatBuffer(ready_texcoords);
+        fbVertexNormals = makeFloatBuffer(ready_normals);
+
     }
+
     public static FloatBuffer makeFloatBuffer(float[] arr) {
         FloatBuffer fb = BufferUtils.createFloatBuffer(4*arr.length);
         fb.put(arr).flip();
